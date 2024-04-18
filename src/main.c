7c37e18088ef3f6d8c07c3e63f70641977f0eac8
ppodobinski <peterpodobinski2024@u.northwestern.edu>
2024-04-18 17:54:04 -0500

    figured it out

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-int main()
-{
-  stdio_init_all();
-  if (cyw43_arch_init())
-  {
-    printf("Wi-Fi init failed");
-    return -1;
-  }
-  while (true)
-  {
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-    sleep_ms(250);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-    sleep_ms(250);
-  }
+#include "pico/stdlib.h"
+
+int main() {
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+    }
+#endif
 }
